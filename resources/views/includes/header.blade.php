d6af895b0aece0523f66ff60286638a534f5ec23
mmoshihud <18-39125-3@student.aiub.edu>
2022-03-11 00:23:20 +0600

    add footer, logo fix, css fix

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -2,12 +2,12 @@
     <img class="logo" src="/images/premiere.svg" alt="logo" height="60" width="60" />
     <h1 class="heading">emiere Mart</h1>
     <nav class="nav-links">
-        <a href="#">Best Seller</a>
-        <a href="#">About</a>
+        <a class="nav-item" href="#">Best Seller</a>
+        <a class="nav-item" href="#">About</a>
         <input type="search" name="search" placeholder="Search everything at premire mart"
             style="height: 50px; width: 500px" />
-        <a href="#" class="cta"><button>Sing IN</button></a>
-        <a href="#" class="cta"><button>Contact</button></a>
-        <a href="#" class="cta"><button>Cart</button></a>
+        <a class="cta" href="#"><button>Profile</button></a>
+        <a class="cta" href="#"><button>Log in</button></a>
+        <a class="cta" href="#"><button>Cart</button></a>
     </nav>
 </div>
