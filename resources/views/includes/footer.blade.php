6e7ca2cd2f0159c3556948ba0fd5f465e9aa5629
mmoshihud <18-39125-3@student.aiub.edu>
2022-03-10 22:09:56 +0600

    Create nav bar and product category

---
+++ resources/views/includes/footer.blade.php
@@ -0,0 +1,12 @@
+<div class="footer-div">
+    <footer class="ft-div">
+        <ul>
+            <li>All Department</li>
+            <li>About Us</li>
+            <li>Careers</li>
+            <li>Help</li>
+            <li>Contact US</li>
+        </ul>
+        <p>&copy; 2042 Premere Mart. All right reserved</p>
+    </footer>
+</div>
---
+++ resources/views/includes/header.blade.php
@@ -0,0 +1,13 @@
+<div class="global-header">
+    <img class="logo" src="/images/premiere.svg" alt="logo" height="60" width="60" />
+    <h1 class="heading">emiere Mart</h1>
+    <nav class="nav-links">
+        <a href="#">Best Seller</a>
+        <a href="#">About</a>
+        <input type="search" name="search" placeholder="Search everything at premire mart"
+            style="height: 50px; width: 500px" />
+        <a href="#" class="cta"><button>Sing IN</button></a>
+        <a href="#" class="cta"><button>Contact</button></a>
+        <a href="#" class="cta"><button>Cart</button></a>
+    </nav>
+</div>
